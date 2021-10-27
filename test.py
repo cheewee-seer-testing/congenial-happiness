7b9984ba84b70f59d0894f8aa18f1be7e0903564
Chee Wee Phua <cheewee@seermedical.com>
2021-10-27 15:26:18 +1100

    Testing 1

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 print(eval('1 + 2'))
+
+import subprocess
+
+def transcode_file(request, filename):
+    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
+    subprocess.call(command, shell=True)  # a bad idea!
